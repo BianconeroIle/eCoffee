f1fcf6f78ab26b8cd330ef139dd53f7703d8fb11
Vlade Ilievski <vlade.i@uxmake.com>
2016-08-01 16:13:54 +0200

    quantity spinner is set

--- app/src/main/java/com/ecoffee/ecoffee/ui/MakeOrderCustomDialog.java
+++ app/src/main/java/com/ecoffee/ecoffee/ui/MakeOrderCustomDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
@@ -16,7 +17,10 @@ import com.ecoffee.ecoffee.R;
 import com.ecoffee.ecoffee.util.AppUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.ecoffee.ecoffee.R.layout.support_simple_spinner_dropdown_item;
 
 /**
  * Created by Vlade Ilievski on 7/28/2016.
@@ -24,8 +28,8 @@ import java.util.List;
 public class MakeOrderCustomDialog extends Dialog implements View.OnClickListener {
     Button buttonNO;
     Button buttonYES;
-    Spinner spinner;
-    Spinner spinner1;
+    Spinner productsSpinner;
+    Spinner quantitySpinner;
     String getProducts;
     Context context;
     TextView produtsTextView;
@@ -44,15 +48,24 @@ public class MakeOrderCustomDialog extends Dialog implements View.OnClickListene
 
         buttonYES = (Button) findViewById(R.id.buttonYES);
         buttonNO = (Button) findViewById(R.id.buttonNO);
-        produtsTextView =(TextView)findViewById(R.id.productsTextView);
-        quantityTextView=(TextView)findViewById(R.id.quantityTextView);
+        produtsTextView = (TextView) findViewById(R.id.productsTextView);
+        quantityTextView = (TextView) findViewById(R.id.quantityTextView);
         buttonYES.setOnClickListener(this);
         buttonNO.setOnClickListener(this);
 
+    /*    productsSpinner=(Spinner)findViewById(R.id.productsSpinner);
+        ArrayAdapter<String> dataAdapterNumbers = new ArrayAdapter<String>(this,AppUtil.getProducts(),R.la);
+        dataAdapterNumbers.setDropDownViewResource(support_simple_spinner_dropdown_item);
+        productsSpinner.setAdapter(dataAdapterNumbers);*/
+
+        quantitySpinner=(Spinner)findViewById(R.id.quantitySpinner);
+        /*List<Integer> integers=new ArrayList<>();
+
+        ArrayAdapter<Integer> dataAdapterQuantity = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item,);
+        dataAdapterQuantity.setDropDownViewResource((support_simple_spinner_dropdown_item));
+        quantitySpinner.setAdapter(dataAdapterQuantity);*/
 
     }
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -64,24 +77,6 @@ public class MakeOrderCustomDialog extends Dialog implements View.OnClickListene
                 break;
         }
     }
-
-
-
-//
-//    spinner1=(Spinner)
-//
-//    findViewById(R.id.spinner1);
-//
-//    ArrayAdapter<String> dataAdapterNumbers = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item);
-//    dataAdapterNumbers.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-//    spinner1.setAdapter(dataAdapterNumbers);
-//
-//    buttonNO=(Button)
-//
-//    findViewById(R.id.buttonNO);
-
-
-
 
 }
 
